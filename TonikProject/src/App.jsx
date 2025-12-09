f62e16ca6edbc830cef6397447f8c117ce28b949
арбузный ебака <lev.fedorov87@gmail.com>
2025-12-09 17:17:47 +0200

    Bug fix + adaptive

--- TonikProject/src/App.jsx
+++ TonikProject/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
         <>
             <Routes>
 
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
 
 
-                <Route path="/" element={<TonicsPage />} />
+                <Route path="/tonics" element={<TonicsPage />} />
             </Routes>
         </>
     )
